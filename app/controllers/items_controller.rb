1aba1b27688ee1af02318dc3926f72a74676fda9
stanri69 <stanri69@ukr.net>
2021-12-22 12:28:45 +0200

    created edit and show

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,5 @@
 class ItemsController < ApplicationController
+  before_action :find_item, only: [:show, :edit, :update, :destroy]
 
   def index
     @items = Item.all
@@ -25,14 +26,25 @@ class ItemsController < ApplicationController
   end
 
   def update
+    if @item.update(item_params)
+      redirect_to item_path(@item)
+    else
+      render 'edit'
+    end
   end
 
   def destroy
+    @item.destroy
+    redirect_to root_path
   end
 
   private
 
   def item_params
     params.require(:item).permit(:type_dish, :name, :description, :price)
+  end
+
+  def find_item
+    @item = Item.find(params[:id])
   end
 end
